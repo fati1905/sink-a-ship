ac69f3339a1e20e62ab7146a95bbd7ca5109a3ee
fati1905 <fatimabenkadourr@gmail.com>
2022-08-01 17:59:08 +0200

    Ship Class

--- Ship.java
+++ Ship.java
@@ -1,44 +1,41 @@
 public class Ship{
 
-	int row; //The row of the ship
-	int col; //The column of the stern of the ship (Back of the ship)
-	int colBow; //The column of the bow of the ship (Front of the ship)
+	ArrayList<Integer> loc;
 
-	//Instanciate the ship location
+	//Instantiate the ship location
 	Ship(){
 		
-		this.row = 1;
-		this.col = (int) Math.floor(Math.random()*5+4); //Generate a rondom location for the ship (from 0 to 4, The 4 is because the bow of the ship is three block bigger than the column of the stern)
-		this.colBow = col + 3; //The length of the ship is 3 block so we add that to column of the stern (col) of the ship to get the column of the bow (colBow) of the ship
+		int rand = (int)Math.floor(Math.random*5+4);
+		loc = new ArrayList(); 
+		loc.add(rand);
+		loc.add(rand+1);
+		loc.add(rand+2);
 	}
 
 
-	//Setting or updating the location of the ship
-	public void setLocation(int r2, int c2 ){
-
-		this.row = 1;
-		this.col = (int) Math.floor(Math.random()*5+4); //regenerate a new location 
-		this.colBow = this.col + 3;
-	}
+	public void setLocationCells(ArrayList<Integer> locationCells){
+       this.loc =  locationCells;
+    }
 
 	//Check if the ship was hit or not
-	public boolean isHit(int rowHit, int colHit){
+	public String isHit(int num){
 
-		if(rowHit == row){
+		//We recieve the number hit in num
 
-			if(col<= colHit <= colBow){
-				
-				return true;
+		String result = "miss"; //The result of the hit is st on miss on default
+
+		if(loc.contains(num)){ //if The number inserted is in the arrayList
 			
-			}else{
+			result = "hit";//Then The ship was hit
 
-				return false;
+			loc.remove(loc.indexOf(num));//We remove the hit block from the arraylist
+
+			if(loc.isEmpty()){ //Check if the ArrayList is not empty
+				result = "kill"; //If it is we should end the game
 			}
-
-		}else{
-
-			return false;
 		}
+
+		return result;
 	}
 
 }
