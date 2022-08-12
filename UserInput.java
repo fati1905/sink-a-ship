d5e9e1de5b0afc7b6e606a1172a70ece7d109183
fati1905 <fatimabenkadourr@gmail.com>
2022-08-12 19:21:53 +0200

    next line removed

--- UserInput.java
+++ UserInput.java
@@ -9,7 +9,7 @@ public class UserInput {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static String readLine() {
-        System.out.println("prompt ");
+        System.out.print("prompt: ");
         return scanner.nextLine();
     }
 }
