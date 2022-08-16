0d6f75fd8c70cd6862061f27e52f3a64362a8b22
fati1905 <fatimabenkadourr@gmail.com>
2022-08-16 16:44:40 +0200

    Updated the setter

--- Ship.java
+++ Ship.java
@@ -8,7 +8,7 @@ import java.util.Collections;
  */ 
 
 public class Ship {
-    protected ArrayList<Point> loc ;
+    protected ArrayList<Point> loc;
     protected ArrayList<Point> ships = new ArrayList<Point>();
     protected int orientation;//Horizontal: 0, Vertical: 1
 
@@ -19,32 +19,43 @@ public class Ship {
         orientation = (int) Math.floor(Math.random()*2);
     }
 
-    public void setShip(int width){
+    public void setShip(int width, int grid){
         //TODO: Change the grid to a variable
         Point cord = new Point(0,0);
+        boolean flag = false;
 
-        while(ships.contains(cord)){
+        while(!flag){
 
             if(this.orientation%2==0){
-                cord.y = (int) Math.floor(Math.random()*(11));
-                cord.x = (int) Math.floor(Math.random()*(11-width));
+                cord.y = (int) Math.floor(Math.random()*(grid));
+                cord.x = (int) Math.floor(Math.random()*(grid-width));
             }else {
-                cord.y = (int) Math.floor(Math.random()*(11-width));
-                cord.x = (int) Math.floor(Math.random()*(11));
+                cord.y = (int) Math.floor(Math.random()*(grid-width));
+                cord.x = (int) Math.floor(Math.random()*(grid));
             }
-        }
-        //Add to the ArrayList (ship) the position of the ship
-        for(int i =0; i<width; i++){
-            loc.add(cord);
-            //If the ship is set on horizontal then y remains the same and only x gets incremented in every iteration
-            if (orientation % 2 == 0) {
-                cord.x++;
-            } else{
-                //Otherwise, if the ship is vertical then x remains static and y increments
-                cord.y++;
+            //Add to the ArrayList (ship) the position of the ship
+            for(int i =0; i<width; i++){
+                loc.add(cord);
+                //If the ship is set on horizontal then y remains the same and only x gets incremented in every iteration
+                if (orientation % 2 == 0) {
+                    cord.x++;
+                } else{
+                    //Otherwise, if the ship is vertical then x remains static and y increments
+                    cord.y++;
+                }
             }
-            i++;
+
+            //Check if the ships don't overlap
+
+            for(int i = 0; i<width; i++){
+                if(ships.contains(loc.get(i))){
+                    flag = true;
+                    break;
+                }
+            }
+
         }
+
     }
 
     //Test method useful for debugging. Please remove it to hide the position of the ship
